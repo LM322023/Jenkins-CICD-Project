b12917b7ef5a55375758f861d2fda4d14ae3b22b
tahmichael25 <tahmichael_5@yahoo.co.uk>
2023-10-28 11:50:09 -0400

    update to Prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,8 +1,8 @@
 sudo useradd --no-create-home prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
- 
-wget  https://github.com/prometheus/prometheus/releases/download/v2.23.0/prometheus-2.23.0.linux-amd64.tar.gz
+
+wget https://github.com/prometheus/prometheus/releases/download/v2.23.0/prometheus-2.23.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.23.0.linux-amd64.tar.gz
 sudo cp prometheus-2.23.0.linux-amd64/prometheus /usr/local/bin
 sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin
@@ -10,10 +10,11 @@ sudo cp -r prometheus-2.23.0.linux-amd64/consoles /etc/prometheus/
 sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
 sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
 
-rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+# Add these lines to create and copy the configuration and service files
 sudo cp prometheus.yml /etc/prometheus/
 sudo cp prometheus.service /etc/systemd/system/prometheus.service
 
+rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
 sudo chown prometheus:prometheus /etc/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
@@ -21,9 +22,8 @@ sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 sudo chown -R prometheus:prometheus /var/lib/prometheus
 
+# Restart and enable the service
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
-
-
